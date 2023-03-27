c8797d67f7f18112bab795ae47400a9a44b8b13b
F-hiller <jjun3511@naver.com>
2023-03-27 23:55:47 +0900

    Initial commit

---
+++ src/main/java/com/haedal/fhiller/FhillerApplication.java
@@ -0,0 +1,13 @@
+package com.haedal.fhiller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhillerApplication.class, args);
+	}
+
+}
